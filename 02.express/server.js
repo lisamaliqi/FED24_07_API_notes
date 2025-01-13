1173afefa114f8bf9041c4da74c7685cdec93513
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-13 17:41:47 +0100

    request on specific routes

--- 02.express/server.js
+++ 02.express/server.js
@@ -3,12 +3,28 @@
  */
 
 
-// Require express
+// Require eller hämta ut express, sen ge PORT ett nummer på 3500 för localhost
 const express = require("express");
-const PORT = 3000;
-// Create a new Express app
+const PORT = 3500;
+// Skapa en ny express-app
 const app = express();
-// Start listening for incoming requests on port 3000
+
+// Lyssna efter inkommande GET requests till "/"
+// om man går in i localHost:3400 så kommer detta loggas i terminalen 
+app.get("/", (req, res) => {
+	console.log("Someone requested my (g)root 🌲");
+	console.log("Requested method:", req.method);
+	console.log("Requested path:", req.path);
+	res.send("Oh, hi there 😊");
+});
+
+// Om man går in i localHost:3500/lol så kommer detta visas på sidan
+app.get("/lol", (req, res) => {
+	res.send("I was wondering why the frisbee kept getting bigger and bigger, but then it hit me.");
+});
+
+
+// Starta lyssningen av inkommande request på port 3500
 app.listen(PORT, () => {
 	// Will be invoked once the server has started listening
 	console.log(`🥳 Yay, server started on localhost:${PORT}`);
