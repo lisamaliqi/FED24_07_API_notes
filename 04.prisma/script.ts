3f9a69f90719f38026f8a5d1c05d6ec14ccdb948
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-23 14:20:28 +0100

    writing first query in prisma to see if it works

--- 04.prisma/script.ts
+++ 04.prisma/script.ts
@@ -5,6 +5,11 @@ const prisma = new PrismaClient();
 async function main() {
 	// ... you will write your Prisma Client queries here
 	console.log("It works?");
+
+    // Hämta ut alla phones från databasen och logga ut det i konsollen med prisma 
+	// SELECT * FROM phones
+	const phones = await prisma.phones.findMany();
+	console.log("Phones:", phones);
 }
 
 main()
