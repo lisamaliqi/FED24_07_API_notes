ad8586a80c8120cef0e7b0c7616de51be12a27da
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-26 16:58:47 +0100

    moved function handlePrismaError to prisma folder to make app.ts more readable

--- 05.books/src/app.ts
+++ 05.books/src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import prisma from "./prisma"; // importing the prisma instance we created
 import morgan from "morgan";
-import { PrismaClientInitializationError, PrismaClientKnownRequestError, PrismaClientValidationError } from "@prisma/client/runtime/library";
 import { stat } from "fs";
 import { error } from "console";
 
@@ -9,26 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
-//funktion som hanterar errors så man slipper skriva error-kod för varje request
-const handlePrismaError = (err: unknown) => {
-	if (err instanceof PrismaClientInitializationError) {
-		return { status: 500, message: "Error initializing connection to database" };
-
-	} else if (err instanceof PrismaClientKnownRequestError) {
-		if (err.code === "P2021") {
-			return { status: 500, message: "Database table does not exist" };
-
-		} else if (err.code === "P2025") {
-			return { status: 404, message: "Not Found" };
-		}
-
-	} else if (err instanceof PrismaClientValidationError) {
-		return { status: 400, message: "Validation Error" };
-
-	}
-
-	return { status: 500, message: "Something went wrong when querying the database" };
-}
+//skicka funktionen handlePrismaError till exception/prisma.ts 
 
 /**
  * GET /
---
+++ 05.books/src/exceptons/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaClientInitializationError, PrismaClientKnownRequestError, PrismaClientValidationError } from "@prisma/client/runtime/library";
+
+//funktion som hanterar errors så man slipper skriva error-kod för varje request
+const handlePrismaError = (err: unknown) => {
+    if (err instanceof PrismaClientInitializationError) {
+        return { status: 500, message: "Error initializing connection to database" };
+
+    } else if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === "P2021") {
+            return { status: 500, message: "Database table does not exist" };
+
+        } else if (err.code === "P2025") {
+            return { status: 404, message: "Not Found" };
+        }
+
+    } else if (err instanceof PrismaClientValidationError) {
+        return { status: 400, message: "Validation Error" };
+
+    }
+
+    return { status: 500, message: "Something went wrong when querying the database" };
+}
