abb081697ebc1aff1cd6eaaf1b4e79d572e15c1a
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-27 13:19:01 +0100

    made validation error message better

--- 05.books/src/routes/author.ts
+++ 05.books/src/routes/author.ts
@@ -27,8 +27,12 @@ router.get("/:authorId", show);
  * Create a author
  */
 router.post("/", [
-	body("name").isString().isLength({ min: 3, max: 191 }),
-	body("birthyear").optional().isInt(),
+	body("name")
+		.isString().withMessage("has to be a string").bail()
+		.isLength({ min: 3, max: 191 }).withMessage("has to be 3-191 chars long"),
+	body("birthyear")
+		.optional()
+		.isInt().withMessage("has to be a integer"),
 ], store);
 
 
