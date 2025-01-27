e0e59c464f131f9be38ea3d88522fdf0e6370d12
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-27 14:03:12 +0100

    adding trim() function to creating an author and adding their name

--- 05.books/src/routes/author.ts
+++ 05.books/src/routes/author.ts
@@ -29,7 +29,7 @@ router.get("/:authorId", show);
 router.post("/", [
 	body("name")
 		.isString().withMessage("has to be a string").bail()
-		.isLength({ min: 3, max: 191 }).withMessage("has to be 3-191 chars long"),
+		.trim().isLength({ min: 3, max: 191 }).withMessage("has to be 3-191 chars long"),
 	body("birthyear")
 		.optional()
 		.isInt().withMessage("has to be a integer"),
