02337cfc5e71c1499756eb38f7ec27555c887813
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-28 14:22:34 +0100

    added a user service and moved getting of user by email to that service

---
+++ 05.books/src/services/user_service.ts
@@ -0,0 +1,13 @@
+/**
+ * User service 
+ */
+
+import prisma from "../prisma"
+
+export const getUserByEmail = async (email: string) => {
+    return await prisma.user.findUnique({
+        where: {
+            email: email,
+        },
+    });
+};
--- 05.books/src/validations/user_rules.ts
+++ 05.books/src/validations/user_rules.ts
@@ -3,6 +3,8 @@
  */
 
 import { body } from "express-validator";
+import prisma from "../prisma";
+import { getUserByEmail } from "../services/user_service";
 
 export const createUserRules = [
     //name string, trimmed + between 3-191 chars
@@ -13,11 +15,22 @@ export const createUserRules = [
 
     //email required, string,  valid email unique
     body('email')
-        .trim().isEmail().withMessage('email has to be a valid email'),
+        .trim().isEmail().withMessage('email has to be a valid email').bail()
+        .custom(async (value: string) => {
+            // på något sätt få tillgång till vad värdet av email är 
+            // samt också kolla upp det i databasen
+            const user = await getUserByEmail(value);
+
+            // samt berätta express validator ifall värdet klarar validation eller inte
+            if(user) {
+                // user finns redan? skicka ERROROROROOROR
+                // return Promise.reject('Email already exists');
+                throw new Error('Email already exists');
+            }
+        }),
 
     //password required, string,  at least 6 chars
     body('password')
         .isString().withMessage('password has to be a string')
-        .isLength({ min: 6 }).withMessage('password has to be at least 6 chars'),
-        
+        .isLength({ min: 6 }).withMessage('password has to be at least 6 chars'),    
 ];
