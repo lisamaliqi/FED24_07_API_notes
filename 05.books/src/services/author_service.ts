1d3b20046de41cc9d7dd8f338fead2a12a9d40a1
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-29 11:43:14 +0100

    adding author service skeleton

---
+++ 05.books/src/services/author_service.ts
@@ -0,0 +1,44 @@
+/**
+ * Author Service
+ */
+import prisma from "../prisma";
+
+/**
+ * Get all authors
+ */
+export const getAuthors = async () => {
+}
+
+/**
+ * Get a single author
+ *
+ * @param authorId The ID of the Author to get
+ */
+export const getAuthor = async (authorId: number) => {
+}
+
+/**
+ * Create an author
+ *
+ * @param data Author data
+ */
+export const createAuthor = async (data) => {
+}
+
+/**
+ * Update an author
+ *
+ * @param authorId The ID of the Author to update
+ * @param data Author data
+ * @returns
+ */
+export const updateAuthor = async (authorId: number, data) => {
+};
+
+/**
+ * Delete an author
+ *
+ * @param authorId The ID of the Author to delete
+ */
+export const deleteAuthor = async (authorId: number) => {
+}
