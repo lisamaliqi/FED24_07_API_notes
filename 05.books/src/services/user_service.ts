6dd06af0530076229ac5dfcc6a64c9d5da6c19ec
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-30 09:52:26 +0100

    small comment in the code

--- 05.books/src/services/user_service.ts
+++ 05.books/src/services/user_service.ts
@@ -4,6 +4,11 @@
 
 import prisma from "../prisma"
 
+/**
+ * Get a User by email
+ *
+ * @param email Email of user to get
+ */
 export const getUserByEmail = async (email: string) => {
     return await prisma.user.findUnique({
         where: {
