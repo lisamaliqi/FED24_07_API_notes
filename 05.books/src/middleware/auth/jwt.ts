6292821a0915004ce3a89ec34f7aae164f3cdf2e
lisamaliqi <lisamaliqi@hotmail.com>
2025-02-11 17:22:37 +0100

    finishing up step 4-6

--- 05.books/src/middleware/auth/jwt.ts
+++ 05.books/src/middleware/auth/jwt.ts
@@ -3,9 +3,14 @@
  */
 import Debug from "debug";
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JwtAccessTokenPayload } from "../../types/JWT.types";
 
 // Create a new debug instance
 const debug = Debug("prisma-books:jwt");
+
+// Get environment variables
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
 
 export const validateAccessToken = async (req: Request, res: Response, next: NextFunction) => {
@@ -33,11 +38,29 @@ export const validateAccessToken = async (req: Request, res: Response, next: Nex
 
 
 	// 4. Verify token and extract payload, otherwise bail 🛑
+    if (!ACCESS_TOKEN_SECRET) {
+		debug("🛑🛑🛑 ACCESS_TOKEN_SECRET missing in environment");
+		res.status(500).send({ status: "error", message: "No access token secret defined" });
+		return;
+	};
 
+	try {
+		const payload = jwt.verify(token, ACCESS_TOKEN_SECRET) as unknown as JwtAccessTokenPayload;  // stupid TypeScript
+		debug("Payload: %O", payload);
 
-	// 5. Attach payload to request
+        // 5. Attach payload to request
+		req.user = {
+			id: payload.sub,
+			name: payload.name,
+			email: payload.email,
+		};
 
-    
-	// 6. Profit 💰🤑
-	next();
-}
+		// 6. Profit 💰🤑
+		next();
+        
+	} catch (err) {
+		debug("JWT Verify failed: %O", err);
+		res.status(401).send({ status: "fail", message: "Authorization denied" });
+		return;
+	};
+};
