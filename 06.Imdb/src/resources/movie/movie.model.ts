24f46edf623591628c4660e9c12a1d6198baa16f
lisamaliqi <lisamaliqi@hotmail.com>
2025-02-12 14:05:04 +0100

    Created MovieDocument, MovieSchema and Movie model.

---
+++ 06.Imdb/src/resources/movie/movie.model.ts
@@ -0,0 +1,22 @@
+import { model, Schema, Document } from "mongoose";
+
+export interface MovieDocument extends Document {
+	title: string;
+	runtime?: number;
+	release_year?: number;
+};
+
+const MovieSchema: Schema = new Schema<MovieDocument>({
+	title: {
+		type: Schema.Types.String,
+		required: true,
+	},
+	runtime: {
+		type: Schema.Types.Int32,
+	},
+	release_year: {
+		type: Schema.Types.Int32,
+	},
+});
+
+export const Movie = model("Movie", MovieSchema);
