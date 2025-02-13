7104ae79e4a300bdcbdda56b50f39c11bf522a9e
lisamaliqi <lisamaliqi@hotmail.com>
2025-02-13 10:43:04 +0100

    mongoose schema sanitization, validation, custom validation and default values

--- 06.Imdb/src/resources/movie/movie.model.ts
+++ 06.Imdb/src/resources/movie/movie.model.ts
@@ -2,8 +2,10 @@ import { model, Schema, Document } from "mongoose";
 
 export interface MovieDocument extends Document {
 	title: string;
-	runtime?: number;
-	release_year?: number;
+	runtime: number | null;
+	release_year: number | null;
+    genres: string[];
+    watched: Date;
 };
 
 const currentYear = new Date().getFullYear();
@@ -12,16 +14,36 @@ const MovieSchema = new Schema<MovieDocument>({
 	title: {
 		type: String,
 		required: true,
+        trim: true,
+        min: [2, 'Has to be at least 2 chars']
 	},
 	runtime: {
 		type: Number,
-        min: [1, 'Need to have a positive runtime'],
+        default: null,
+        // min: [1, 'Need to have a positive runtime'],
+        validate(value: number | null) {
+            if (value !== null && value < 1 ) {
+                throw new Error('just because you thought a movie was bad, it should not have zero or negative runtime');
+            };
+        },
 	},
 	release_year: {
 		type: Number,
+        default: null,
         min: [1888, 'Needs to be 1888 or later'],
         max: [currentYear, 'Cannot be in the future'],
 	},
+    genres: {
+        type: [String],
+        default: [],
+        lowercase: true,
+    },
+    watched: {
+        type: Date,
+        default() {
+            return Date.now();
+        },
+    },
 });
 
 export const Movie = model("Movie", MovieSchema);
