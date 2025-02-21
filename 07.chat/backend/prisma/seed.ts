b6b894e3d7792a862aab8fd782914aa9aace110e
lisamaliqi <lisamaliqi@hotmail.com>
2025-02-21 11:26:18 +0100

    added room seeder

--- 07.chat/backend/prisma/seed.ts
+++ 07.chat/backend/prisma/seed.ts
@@ -3,6 +3,36 @@ const prisma = new PrismaClient();
 
 async function main() {
 	// Here be all your seeds 🌱
+    // upsert = create or update a room
+    await prisma.room.upsert({
+        where: {
+            name: 'Captain', //look after this name
+        },
+        update: {}, //if name exists, update it
+        create: { 
+            name: 'Captain', //if name doesn't exist, create one named Captain
+        },
+    });
+
+    await prisma.room.upsert({
+        where: {
+            name: 'General',
+        },
+        update: {},
+        create: {
+            name: 'General',
+        },
+    });
+
+    await prisma.room.upsert({
+        where: {
+            name: 'Major',
+        },
+        update: {},
+        create: {
+            name: 'Major',
+        },
+    });
 }
 
 main()
