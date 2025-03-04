e149c92cf435978f0072c35e665eb897ad4e58e7
lisamaliqi <lisamaliqi@hotmail.com>
2025-03-04 10:50:41 +0100

    created GraphQL-server, a type definition for a book + the first query and resolver for the query

--- 08.graphQL/backend/src/graphql/resolvers.ts
+++ 08.graphQL/backend/src/graphql/resolvers.ts
@@ -1,6 +1,12 @@
 // Resolvers define how to fetch the types defined in your schema.
+import prisma from "../prisma";
 
 const resolvers = {
-}
+    Query: {
+        books: () => {
+            return prisma.book.findMany();
+        },
+    },
+};
 
 export default resolvers;
--- 08.graphQL/backend/src/server.ts
+++ 08.graphQL/backend/src/server.ts
@@ -1,7 +1,33 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone';
 import * as dotenv from "dotenv";
+import resolvers from "./graphql/resolvers";
+import typeDefs from "./graphql/typeDefs";
 
 // Initialize dotenv so it reads our `.env`-file
 dotenv.config();
 
 // Read port to start server on from `.env`, otherwise default to port 4000
 const PORT = Number(process.env.PORT) || 4000;
+
+
+// Set up the Apollo Server
+const server = new ApolloServer({
+    resolvers: resolvers,
+    typeDefs: typeDefs,
+});
+
+
+// Start a standalone server on the specified port
+startStandaloneServer(server, {
+        listen: {
+            port: PORT,
+        },
+    })
+    .then(props => {
+        console.log(`🚀GraphQL server ready at: ${props.url}`);
+    })
+    .catch(err => {
+        console.error(`📛GraphQL server could not start because: `, err);
+        process.exit(1);
+    });
